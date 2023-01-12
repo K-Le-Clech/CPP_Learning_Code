bb788ff24037bff39851b45bb777dc1362adfe94
Kevin LE CLECH <kevin.leclech09@gmail.com>
2023-01-12 15:55:38 +0100

    chap1 exo3 partie2

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << array[i] << std::endl;
+    if(argc != 2){
+        std::cerr <<"Pas le bon nombre d'argument"<<std::endl;
+        return -1;
     }
+    std::cout<<argv[1]<<std::endl;
+
+    int number = std::stoi(argv[1]);
+
+    
+    /*int array[50] = {};
+    int cmp = 1;
+
+    for (int i = 0; i<50; i++)
+    {
+        array[i] = i+1;
+    }*/
+    
 
     return 0;
 }
